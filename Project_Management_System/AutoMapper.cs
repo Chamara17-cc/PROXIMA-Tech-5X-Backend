94bcfd44d7cf379a48d97942e4095a8acf561f28
Chamara17-cc <149788803+Chamara17-cc@users.noreply.github.com>
2024-03-04 23:19:46 +0530

    Base-Project-Updated

---
+++ Project_Management_System/AutoMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+
+namespace Project_Management_System
+{
+    public class AutoMapper : Profile
+    {
+        public AutoMapper()
+        {
+            //Create Automappers here EX: CreateMap<AddbudgetDTO, Budget>();MAp AddBudget to Budget 
+        }
+    }
+}
---
+++ Project_Management_System/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Project_Management_System.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+
+        }
+        //Add Your DbContext here Ex: public DbSet<Budget> Budgets { get; set; }
+
+    }
+}
