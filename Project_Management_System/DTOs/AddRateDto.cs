184c0a78ce06af89a7c2083a7c02cf45a5ff0239
Chamara17-cc <149788803+Chamara17-cc@users.noreply.github.com>
2024-04-11 13:55:52 +0530

    Rate update

---
+++ Project_Management_System/DTOs/AddRateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Project_Management_System.DTOs
+{
+    public class AddRateDto
+    {
+        public DateTime UpdatedDate { get; set; }
+        public double CurrentRate { get; set; }
+    }
+}
---
+++ Project_Management_System/DTOs/GetRateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Project_Management_System.DTOs
+{
+    public class GetRateDto
+    {
+        public double CurrentRate { get; set; }
+    }
+}
