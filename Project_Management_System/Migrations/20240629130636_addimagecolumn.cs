0e1832f95653e6ea3d414fe39325dd0b7cca70a9
Chamara17-cc <149788803+Chamara17-cc@users.noreply.github.com>
2024-06-29 19:17:13 +0530

    dd

---
+++ Project_Management_System/Migrations/20240629130636_addimagecolumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class addimagecolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileImageName",
+                table: "Users");
+        }
+    }
+}
